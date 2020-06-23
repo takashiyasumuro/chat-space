ecb817867b44902d050d98466bf5a22147b4a319
takashiyasumuro <66367722+takashiyasumuro@users.noreply.github.com>
2020-06-23 14:50:45 +0900

    修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,7 @@ class GroupsController < ApplicationController
   def edit
     @group = Group.find(params[:id])
   end
+  
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
